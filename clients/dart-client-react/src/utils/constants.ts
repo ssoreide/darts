502b7aee00924bedd3db20cb34aedc19b817c2fd
Steinar Søreide <steinar@mjoll.no>
2021-04-04 11:22:11 +0200

    Changed to new backend URL (steinar sin deploy)

--- clients/dart-client-react/src/utils/constants.ts
+++ clients/dart-client-react/src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const BASE_API_URL = 'https://3o11qeqasd.execute-api.eu-north-1.amazonaws.com/';
+export const BASE_API_URL = 'https://n6ha2mc9qc.execute-api.eu-north-1.amazonaws.com/';
 export const APP_TITLE = 'Darts 2.0';
 export const POLLING_INTERVAL_IN_MS = 5000;
